d3ebc6815f29a72debdfe2ec040f4bbbe53e2cfc
Deep7133 <deepmenpara9@gmail.com>
2025-05-12 08:38:49 +0530

    added

--- day-6/goodinteger.py
+++ day-6/goodinteger.py
@@ -1,13 +1,13 @@
 size=int(input("enter the size of array "))
 arr=list(map(int, input("enter the numbers ").split()))
-count=0
-for num in arr:
-    x=min(arr)
-    y=num
-    if x**3+y**3==num:
-        print(num)
-    elif x**3+y**3<num:
-        x+=1
-    elif x**3+y**3>num:
-        y-=1
-print(num)
+for i in arr:
+    x=1
+    y=int(i**(1/3))
+    while x<=y:
+        if x**3+y**3==i:
+            print(i)
+            break
+        elif x**3+y**3<i:
+            x+=1
+        elif x**3+y**3>i:
+            y-=1
